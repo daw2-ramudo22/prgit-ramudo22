cb26845abe9b9a735655903bd1c09a2d01b11287
daw2-ramudo22 <ramudocruzalejandro@fpllefia.com>
2021-10-14 16:35:09 +0200

    prgit-ramudo22 v2

--- mitja.c
+++ mitja.c
@@ -13,7 +13,7 @@ int main() {
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
 		printf("ramudo22 Vols continuar (s/n): ");
-		scanf(" %c",&consulta); 
+		scanf(" %s",&consulta); 
 	}
 	mitja=suma/numero_repeticions;
 	printf("ramudo22 La mitja és: %.2f\n",mitja);	
